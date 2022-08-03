2a049096e95ca6752e59e31104dd144fd4a9ce93
Jason Patton <jpatton@cs.wisc.edu>
2022-08-03 11:27:56 -0500

    Do HA query of MIPS

--- store_mips.py
+++ store_mips.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import elasticsearch
 import json
 
-pool = "cm-1.ospool.osg-htc.org"
+pool = "cm-1.ospool.osg-htc.org,cm-2.ospool.osg-htc.org"
 pool_name = "OSPool"
 mips_threshold = 11800
 
